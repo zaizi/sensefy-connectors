753dc74b69ee8ed1f14212eb7af448ce74a82b71
Gunaratnam Kuhajeyan <kgunaratnam@zaizi.com>
2015-06-29 16:12:07 +0530

    feature/SENSEFY-1329 * Adding connectors

---
+++ repository/box/connector/src/main/java/org/apache/manifoldcf/authorities/authorities/box/UserInfo.java
@@ -0,0 +1,20 @@
+package org.apache.manifoldcf.authorities.authorities.box;
+
+public class UserInfo {
+	private String login;
+	private String userId;
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+}
